88ce4527c411ee100105f2c336ef29b09eef8ecb
Berrones Edwin <Berrones.Edwin@hotmail.com>
2023-02-22 16:27:37 -0600

    git init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App () {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>Hello world</h2>
+    </>
+  )
+}
+
+export default App
