564c247008ed2c7702a0ad8c21880d34de806391
Berrones Edwin <Berrones.Edwin@hotmail.com>
2023-02-24 12:06:45 -0600

    feat: new design in sidebar

--- src/components/GeneralSettingsForm.jsx
+++ src/components/GeneralSettingsForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import {
   Box,
   CloseButton,
@@ -11,10 +11,17 @@ import {
   Input,
   Divider,
   Select,
-  Button
+  Button,
+  Switch,
+  VisuallyHidden,
+  Icon,
+  useDisclosure
+
 } from '@chakra-ui/react'
 import useGeneralSettings from '../hooks/useGeneralSettings'
 import { SIZES } from '../constants/sizes'
+import { useUserProvider } from '../hooks/useUserProvider'
+import SettingsImages from './SettingsImages'
 const nameInputs = {
   bgColor: 0,
   headerColor: 1,
@@ -25,6 +32,11 @@ const nameInputs = {
 
 const GeneralSettingsForm = () => {
   const { state, dispatch } = useGeneralSettings()
+  const [allowImage, setAllowImage] = useState(false)
+  const [allowCodeEditor, setAllowCodeEditor] = useState(false)
+  const { setUser } = useUserProvider()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnSettingsImage = useRef()
 
   const handleInputChange = (e) => {
     dispatch({
@@ -33,88 +45,102 @@ const GeneralSettingsForm = () => {
     })
   }
   const handleInputChangeImage = (e) => {
-    // if (e.target.files && e.target.files.length > 0) {
-    //   setUser(e.target.files[0])
-    // }
+    if (e.target.files && e.target.files.length > 0) {
+      setUser({
+        image: {
+          src: e.target.files[0]
+        }
+      })
+    }
   }
   return (
-        <Stack minH={'100vh'} padding={3}>
-            <form onSubmit={(e) => e.preventDefault()}>
+        <>
+            <Stack minH={'100vh'} padding={3}>
+                <form onSubmit={(e) => e.preventDefault()}>
 
-                <FormControl id="bgColor">
-                    <FormLabel>Background Color</FormLabel>
-                    <Input
-                        type="color"
-                        name="bgColor"
-                        value={state.bgColor}
-                        onChange={handleInputChange}
-                    />
-                </FormControl>
-                <FormControl id="bgColor">
+                    <FormControl mb={'1rem'} id="bgColor">
+                        <FormLabel>Background Color</FormLabel>
+                        <Input
+                            type="color"
+                            name="bgColor"
+                            value={state.bgColor}
+                            onChange={handleInputChange}
+                        />
+
+                    </FormControl>
+
                     <FormLabel>Header Color</FormLabel>
-                    <Input
-                        type="color"
-                        name="headerColor"
-                        value={state.bgColorHeader}
-                        onChange={handleInputChange}
-                    />
-                </FormControl>
-                <FormControl id="bgColor">
+                    <FormControl mb={'1rem'} display={'flex'} id="bgColor">
+                        <Input
+                            type="color"
+                            name="headerColor"
+                            value={state.bgColorHeader}
+                            onChange={handleInputChange}
+                        />
+                        <Select
+                            placeholder="Select option"
+                            name="sizeHeader"
+                            onChange={handleInputChange}
+                        >
+                            {SIZES.map((s) => (
+                                <option
+                                    defaultValue={s.size === state.sizeHeader ? s.size : ''}
+                                    value={s.size}
+                                    key={s.id}
+                                >
+                                    {s.size.toLocaleUpperCase()}
+                                </option>
+                            ))}
+                        </Select>
+                    </FormControl>
+
                     <FormLabel>Subtitle Color</FormLabel>
-                    <Input
-                        type="color"
-                        name="subtitleColor"
-                        value={state.bgColorSubtitle}
-                        onChange={handleInputChange}
-                    />
-                </FormControl>
-                <Divider borderRight="1px" w={'1rem'} />
-                <FormControl>
-                    <FormLabel>Size Header</FormLabel>
-                    <Select
-                        placeholder="Select option"
-                        name="sizeHeader"
-                        onChange={handleInputChange}
-                    >
-                        {SIZES.map((s) => (
-                            <option
-                                defaultValue={s.size === state.sizeHeader ? s.size : ''}
-                                value={s.size}
-                                key={s.id}
-                            >
-                                {s.size.toLocaleUpperCase()}
-                            </option>
-                        ))}
-                    </Select>
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Size Subtitle</FormLabel>
-                    <Select
-                        placeholder="Select option"
-                        name="sizeSubtitlte"
-                        onChange={handleInputChange}
-                    >
-                        {SIZES.map((s) => (
-                            <option
-                                defaultValue={s.size === state.sizeHeader ? s.size : ''}
-                                value={s.size}
-                                key={s.id}
-                            >
-                                {s.size.toLocaleUpperCase()}
-                            </option>
-                        ))}
-                    </Select>
-                </FormControl>
-                <FormControl id="Image">
-                    <FormLabel>Add Image</FormLabel>
-                    <Input
-                        type="file"
-                        name="image"
-                        onChange={handleInputChangeImage}
-                    />
-                </FormControl>
-            </form>
-        </Stack>
+                    <FormControl mb={'1rem'} display={'flex'} id="bgColor">
+                        <Input
+                            type="color"
+                            name="subtitleColor"
+                            value={state.bgColorSubtitle}
+                            onChange={handleInputChange}
+                        />
+                        <Select
+                            placeholder="Select option"
+                            name="sizeSubtitlte"
+                            onChange={handleInputChange}
+                        >
+                            {SIZES.map((s) => (
+                                <option
+                                    defaultValue={s.size === state.sizeHeader ? s.size : ''}
+                                    value={s.size}
+                                    key={s.id}
+                                >
+                                    {s.size.toLocaleUpperCase()}
+                                </option>
+                            ))}
+                        </Select>
+                    </FormControl>
+
+                    <FormControl mb={'1rem'} display='flex' alignItems='center'>
+                        <FormLabel htmlFor='allow-image' mb='0' >
+                            Add Image
+                        </FormLabel>
+                        <Switch id='allow-image' isChecked={allowImage} onChange={() => setAllowImage(!allowImage)} />
+                    </FormControl>
+
+                    <FormControl mb={'1rem'} hidden={!allowImage}>
+                        <input onChange={handleInputChangeImage} type="file" name="image" id="image" />
+                        <Button onClick={onOpen} ref={btnSettingsImage} variant='outline' mt={'1rem'} size={'md'}>Settings Image</Button>
+                    </FormControl>
+
+                    <FormControl display='flex' alignItems='center'>
+                        <FormLabel htmlFor='allow-code.editor' mb='0' >
+                            Add Editor
+                        </FormLabel>
+                        <Switch id='allow-code.editor' isChecked={allowCodeEditor} onChange={() => setAllowCodeEditor(!allowCodeEditor)} />
+                    </FormControl>
+                </form>
+            </Stack>
+            <SettingsImages isOpen={isOpen} onClose={onClose} btnRef={btnSettingsImage} />
+        </>
   )
 }
 
