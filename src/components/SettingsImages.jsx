7d408e39405f7aa449f4e8d8b852c511fed14148
Berrones Edwin <Berrones.Edwin@hotmail.com>
2023-02-28 12:57:08 -0600

    feat improvements in side bar image

--- src/components/SettingsImages.jsx
+++ src/components/SettingsImages.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Drawer,
   DrawerBody,
@@ -16,14 +16,20 @@ import {
 import { filters } from '../constants/filter'
 import { useUserProvider } from '../hooks/useUserProvider'
 
-const ImageFilter = ({ filter, src, alt, name, setSettings, settings }) => {
+const ImageFilter = ({ filter, src, name, setSettings, settings }) => {
   return (
     <>
-
-        <Stack onClick={() => setSettings({ filter })} cursor={'pointer'} display={'flex'} justifyContent='center' alignItems={'center'} flexDir='column' className={filter}>
-            <Avatar src={src} size='lg' alt={filter} />
-            <p>{name}</p>
-        </Stack>
+      <Stack onClick={() => setSettings({ filter })}
+            cursor={'pointer'}
+            display={'flex'}
+            justifyContent='center'
+            alignItems={'center'}
+            flexDir='column'
+            className={filter}
+            outline={settings.filter === filter ? '5px solid purple' : ''}>
+        <Image src={src} width='100' height='100' alt={filter} />
+        <Text color={'#rrr'}> {name}</Text>
+      </Stack>
     </>
   )
 }
@@ -47,40 +53,47 @@ const SettingsImages = ({ isOpen, onClose, btnRef }) => {
   }
 
   return (
-        <Drawer
-            isOpen={isOpen}
-            placement='right'
-            onClose={onClose}
-            finalFocusRef={btnRef}
-        >
-            <DrawerOverlay />
-            <DrawerContent>
-                <DrawerCloseButton />
-                <DrawerHeader>Settings Image</DrawerHeader>
+    <Drawer
+      isOpen={isOpen}
+      placement='right'
+      onClose={onClose}
+      finalFocusRef={btnRef}
+    >
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader>Settings Image</DrawerHeader>
 
-                <DrawerBody >
-                <Text>Filters</Text>
-                    <Stack display={'flex'} justifyContent='center' alignItems={'center'} gap='2' flexDir='row' flexWrap={'wrap'} >
+        <DrawerBody >
+          <Text>Filters</Text>
+          <Stack display={'flex'} justifyContent='center' alignItems={'center'} gap='2' flexDir='row' flexWrap={'wrap'} >
 
-                        {
-                            image
-                              ? filters.map((filter) => {
-                                return <ImageFilter settings={settings} setSettings={setSettings} name={filter.name} filter={filter.class} key={filter.name} src={image.src} alt={image.src.lastModified} />
-                              })
-                              : null
-                        }
-                    </Stack>
-                    {/* <Input placeholder='Type here...' /> */}
-                </DrawerBody>
+            {
+              image
+                ? filters.map((filter) => {
+                  return <ImageFilter
+                    setSettings={setSettings}
+                    name={filter.name}
+                    filter={filter.class}
+                    key={filter.name}
+                    src={image.src}
+                    settings={settings}
 
-                <DrawerFooter>
-                    <Button variant='outline' mr={3} onClick={onClose}>
-                        Cancel
-                    </Button>
-                    <Button onClick={handleSettings} colorScheme='blue'>Apply</Button>
-                </DrawerFooter>
-            </DrawerContent>
-        </Drawer>
+                  />
+                })
+                : null
+            }
+          </Stack>
+        </DrawerBody>
+
+        <DrawerFooter>
+          <Button variant='outline' mr={3} onClick={onClose}>
+            Cancel
+          </Button>
+          <Button onClick={handleSettings} colorScheme='blue'>Apply</Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   )
 }
 
